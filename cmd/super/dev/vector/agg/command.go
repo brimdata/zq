brimdata/zed

vam: Initial support for summarize
This commit adds initial support of summarize in the vector runtime.
Currently summarize is limited to use of the count() operator only.

cmd/zed/dev/vector/agg/command.go
@@ -11,7 +11,7 @@ import (
 	"github.com/brimdata/zed/pkg/charm"
 	"github.com/brimdata/zed/pkg/storage"
 	"github.com/brimdata/zed/runtime/vam"
-	"github.com/brimdata/zed/runtime/vam/op"
+	"github.com/brimdata/zed/runtime/vam/op/summarize"
 	"github.com/brimdata/zed/runtime/vcache"
 	"github.com/brimdata/zed/zbuf"
 	"github.com/brimdata/zed/zio"
@@ -70,7 +70,7 @@ func (c *Command) Run(args []string) error {
 	}
 	defer object.Close()
 	//XXX nil puller
-	agg := op.NewCountByString(zed.NewContext(), nil, field)
+	agg := summarize.NewCountByString(zed.NewContext(), nil, field)
 	writer, err := c.outputFlags.Open(ctx, local)
 	if err != nil {
 		return err

runtime/vam/op/summarize/agg.go
@@ -1,4 +1,4 @@
-package op
+package summarize
 
 import (
 	"fmt"
@@ -6,8 +6,103 @@ import (
 
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/runtime/vam/expr"
+	"github.com/brimdata/zed/runtime/vam/expr/agg"
 	"github.com/brimdata/zed/vector"
+	"github.com/brimdata/zed/zcode"
 )
+
+// XXX use zed.Value for slow path stuff, e.g., when the group-by key is
+// a complex type.  when we improve the zed.Value impl this will get better.
+
+// one aggTable per fixed set of types of aggs and keys.
+type aggTable interface {
+	update([]vector.Any, []vector.Any)
+	materialize() vector.Any
+} //XXX
+
+type superTable struct {
+	table   map[string]aggRow  // dont'export
+	aggs    []*expr.Aggregator //XXX don't export
+	builder *vector.RecordBuilder
+}
+
+var _ aggTable = (*superTable)(nil)
+
+type aggRow struct {
+	keys  []zed.Value
+	funcs []agg.Func
+}
+
+func (s *superTable) update(keys []vector.Any, args []vector.Any) {
+	m := make(map[string][]uint32)
+	var n uint32
+	if len(keys) > 0 {
+		n = keys[0].Len()
+	} else {
+		n = args[0].Len()
+	}
+	var keyBytes []byte
+	for slot := uint32(0); slot < n; slot++ {
+		keyBytes = keyBytes[:0]
+		for _, key := range keys {
+			keyBytes = key.AppendKey(keyBytes, slot)
+		}
+		m[string(keyBytes)] = append(m[string(keyBytes)], slot)
+	}
+	for rowKey, index := range m {
+		row, ok := s.table[rowKey]
+		if !ok {
+			row = s.newRow(keys, index)
+			s.table[rowKey] = row
+		}
+		for i, arg := range args {
+			if len(m) > 1 {
+				// If m has only one element we don't have do apply the view
+				// shtuff.
+				arg = vector.NewView(index, arg)
+			}
+			row.funcs[i].Consume(arg)
+		}
+	}
+}
+
+func (s *superTable) newRow(keys []vector.Any, index []uint32) aggRow {
+	var row aggRow
+	for _, agg := range s.aggs {
+		row.funcs = append(row.funcs, agg.Pattern())
+	}
+	var b zcode.Builder
+	for _, key := range keys {
+		b.Reset()
+		key.Serialize(&b, index[0])
+		row.keys = append(row.keys, zed.NewValue(key.Type(), b.Bytes().Body()))
+	}
+	return row
+}
+
+func (s *superTable) materialize() vector.Any {
+	var vecs []vector.Any
+	var tags []uint32
+	// XXX This should reasonably concat all materialize rows together instead
+	// of this crazy Dynamic hack.
+	for _, row := range s.table {
+		tags = append(tags, uint32(len(tags)))
+		vecs = append(vecs, s.materializeRow(row))
+	}
+	return vector.NewDynamic(tags, vecs)
+}
+
+func (s *superTable) materializeRow(row aggRow) vector.Any {
+	var vecs []vector.Any
+	for _, key := range row.keys {
+		vecs = append(vecs, vector.NewConst(key, 1, nil))
+	}
+	for _, fn := range row.funcs {
+		val := fn.Result()
+		vecs = append(vecs, vector.NewConst(val, 1, nil))
+	}
+	return s.builder.New(vecs)
+}
 
 //XXX need to make sure vam operator objects are returned to GC as they are finished
 
@@ -99,9 +194,9 @@ func (c *countByString) countFixed(vec *vector.Const) {
 	val := vec.Value()
 	switch val.Type().ID() {
 	case zed.IDString:
-		c.table[zed.DecodeString(val.Bytes())] += uint64(vec.Length())
+		c.table[zed.DecodeString(val.Bytes())] += uint64(vec.Len())
 	case zed.IDNull:
-		c.nulls += uint64(vec.Length())
+		c.nulls += uint64(vec.Len())
 	}
 }
 

runtime/vam/op/summarize/summarize.go
@@ -1,0 +1,110 @@
+package summarize
+
+import (
+	"github.com/brimdata/zed"
+	"github.com/brimdata/zed/pkg/field"
+	"github.com/brimdata/zed/runtime/vam/expr"
+	"github.com/brimdata/zed/vector"
+)
+
+type Summarize struct {
+	parent vector.Puller
+	zctx   *zed.Context
+	// XX Abstract this runtime into a generic table computation.
+	// Then the generic interface can execute fast paths for simple scenarios.
+	aggs      []*expr.Aggregator
+	aggNames  field.List
+	keyExprs  []expr.Evaluator
+	keyNames  field.List
+	typeTable *zed.TypeVectorTable
+	builder   *vector.RecordBuilder
+
+	types   []zed.Type
+	tables  map[int]aggTable
+	results []aggTable
+}
+
+func New(parent vector.Puller, zctx *zed.Context, aggNames field.List, aggs []*expr.Aggregator, keyNames []field.Path, keyExprs []expr.Evaluator) (*Summarize, error) {
+	builder, err := vector.NewRecordBuilder(zctx, append(keyNames, aggNames...))
+	if err != nil {
+		return nil, err
+	}
+	return &Summarize{
+		parent:    parent,
+		aggs:      aggs,
+		keyExprs:  keyExprs,
+		tables:    make(map[int]aggTable),
+		typeTable: zed.NewTypeVectorTable(),
+		types:     make([]zed.Type, len(keyExprs)),
+		builder:   builder,
+	}, nil
+}
+
+func (s *Summarize) Pull(done bool) (vector.Any, error) {
+	if done {
+		_, err := s.parent.Pull(done)
+		return nil, err
+	}
+	if s.results != nil {
+		return s.next(), nil
+	}
+	for {
+		//XXX check context Done
+		vec, err := s.parent.Pull(false)
+		if err != nil {
+			return nil, err
+		}
+		if vec == nil {
+			for _, t := range s.tables {
+				s.results = append(s.results, t)
+			}
+			s.tables = nil
+			return s.next(), nil
+		}
+		// XXX Eval of keys and vals can be done in parallel.
+		var keys, vals []vector.Any
+		for _, e := range s.keyExprs {
+			keys = append(keys, e.Eval(vec))
+		}
+		for _, e := range s.aggs {
+			vals = append(vals, e.Eval(vec))
+		}
+		vector.Apply(false, func(args ...vector.Any) vector.Any {
+			s.consume(args[:len(keys)], args[len(keys):])
+			// XXX Perhaps there should be a "consume" version of Apply where
+			// no return value is expected.
+			return vector.NewConst(zed.Null, args[0].Len(), nil)
+		}, append(keys, vals...)...)
+	}
+}
+
+func (s *Summarize) consume(keys []vector.Any, vals []vector.Any) {
+	var keyTypes []zed.Type
+	for _, k := range keys {
+		keyTypes = append(keyTypes, k.Type())
+	}
+	tableID := s.typeTable.Lookup(keyTypes)
+	table, ok := s.tables[tableID]
+	if !ok {
+		table = s.newAggTable()
+		s.tables[tableID] = table
+	}
+	table.update(keys, vals)
+}
+
+func (s *Summarize) newAggTable() aggTable {
+	return &superTable{
+		table:   make(map[string]aggRow),
+		aggs:    s.aggs,
+		builder: s.builder,
+	}
+}
+
+func (s *Summarize) next() vector.Any {
+	if len(s.results) == 0 {
+		return nil
+	}
+	t := s.results[0]
+	s.results = s.results[1:]
+	return t.materialize()
+}

vector/recordbuilder.go
@@ -1,0 +1,75 @@
+package vector
+
+import (
+	"slices"
+
+	"github.com/brimdata/zed"
+	"github.com/brimdata/zed/pkg/field"
+)
+
+type RecordBuilder struct {
+	zctx   *zed.Context
+	fields field.List
+	base   *rec
+}
+
+func NewRecordBuilder(zctx *zed.Context, fields field.List) (*RecordBuilder, error) {
+	base := &rec{}
+	for _, path := range fields {
+		if err := addPath(base, path); err != nil {
+			return nil, err
+		}
+	}
+	return &RecordBuilder{zctx: zctx, base: base}, nil
+}
+
+func (r *RecordBuilder) New(vecs []Any) *Record {
+	rec, _ := r.base.build(r.zctx, vecs)
+	return rec
+}
+
+type rec struct {
+	paths []string
+	recs  []*rec
+}
+
+func addPath(r *rec, path field.Path) error {
+	for k, name := range path {
+		idx := slices.Index(r.paths, name)
+		if k == len(path)-1 {
+			if idx > -1 {
+				return &zed.DuplicateFieldError{Name: path.String()}
+			}
+			r.paths = append(r.paths, path[k])
+			r.recs = append(r.recs, nil)
+			return nil
+		}
+		if idx == -1 {
+			idx = len(r.paths)
+			r.paths = append(r.paths, name)
+			r.recs = append(r.recs, &rec{})
+		}
+		if r.recs[idx] == nil {
+			return &zed.DuplicateFieldError{Name: path[:k+1].String()}
+		}
+		r = r.recs[idx]
+	}
+	return nil
+}
+
+func (r *rec) build(zctx *zed.Context, leafs []Any) (*Record, []Any) {
+	var fields []zed.Field
+	var out []Any
+	for i, name := range r.paths {
+		var vec Any
+		if r.recs[i] != nil {
+			vec, leafs = r.recs[i].build(zctx, leafs)
+		} else {
+			vec, leafs = leafs[0], leafs[1:]
+		}
+		fields = append(fields, zed.NewField(name, vec.Type()))
+		out = append(out, vec)
+	}
+	typ := zctx.MustLookupTypeRecord(fields)
+	return NewRecord(typ, out, out[0].Len(), nil), leafs
+}

brimdata/zq

cmd/super/dev/vector/agg/command.go
@@ -11,7 +11,7 @@ import (
 	"github.com/brimdata/super/pkg/charm"
 	"github.com/brimdata/super/pkg/storage"
 	"github.com/brimdata/super/runtime/vam"
-	"github.com/brimdata/super/runtime/vam/op"
+	"github.com/brimdata/super/runtime/vam/op/summarize"
 	"github.com/brimdata/super/runtime/vcache"
 	"github.com/brimdata/super/zbuf"
 	"github.com/brimdata/super/zio"
@@ -70,7 +70,7 @@ func (c *Command) Run(args []string) error {
 	}
 	defer object.Close()
 	//XXX nil puller
-	agg := op.NewCountByString(zed.NewContext(), nil, field)
+	agg := summarize.NewCountByString(zed.NewContext(), nil, field)
 	writer, err := c.outputFlags.Open(ctx, local)
 	if err != nil {
 		return err

runtime/vam/op/summarize/agg.go
@@ -1,13 +1,106 @@
-package op
+package summarize
 
 import (
 	"fmt"
 	"slices"
 
-	"github.com/brimdata/super"
+	zed "github.com/brimdata/super"
 	"github.com/brimdata/super/runtime/vam/expr"
+	"github.com/brimdata/super/runtime/vam/expr/agg"
 	"github.com/brimdata/super/vector"
+	"github.com/brimdata/super/zcode"
 )
+
+// XXX use zed.Value for slow path stuff, e.g., when the group-by key is
+// a complex type.  when we improve the zed.Value impl this will get better.
+
+// one aggTable per fixed set of types of aggs and keys.
+type aggTable interface {
+	update([]vector.Any, []vector.Any)
+	materialize() vector.Any
+}
+
+type superTable struct {
+	table   map[string]aggRow
+	aggs    []*expr.Aggregator
+	builder *vector.RecordBuilder
+}
+
+var _ aggTable = (*superTable)(nil)
+
+type aggRow struct {
+	keys  []zed.Value
+	funcs []agg.Func
+}
+
+func (s *superTable) update(keys []vector.Any, args []vector.Any) {
+	m := make(map[string][]uint32)
+	var n uint32
+	if len(keys) > 0 {
+		n = keys[0].Len()
+	} else {
+		n = args[0].Len()
+	}
+	var keyBytes []byte
+	for slot := uint32(0); slot < n; slot++ {
+		keyBytes = keyBytes[:0]
+		for _, key := range keys {
+			keyBytes = key.AppendKey(keyBytes, slot)
+		}
+		m[string(keyBytes)] = append(m[string(keyBytes)], slot)
+	}
+	for rowKey, index := range m {
+		row, ok := s.table[rowKey]
+		if !ok {
+			row = s.newRow(keys, index)
+			s.table[rowKey] = row
+		}
+		for i, arg := range args {
+			if len(m) > 1 {
+				arg = vector.NewView(index, arg)
+			}
+			row.funcs[i].Consume(arg)
+		}
+	}
+}
+
+func (s *superTable) newRow(keys []vector.Any, index []uint32) aggRow {
+	var row aggRow
+	for _, agg := range s.aggs {
+		row.funcs = append(row.funcs, agg.Pattern())
+	}
+	var b zcode.Builder
+	for _, key := range keys {
+		b.Reset()
+		key.Serialize(&b, index[0])
+		row.keys = append(row.keys, zed.NewValue(key.Type(), b.Bytes().Body()))
+	}
+	return row
+}
+
+func (s *superTable) materialize() vector.Any {
+	var vecs []vector.Any
+	var tags []uint32
+	// XXX This should reasonably concat all materialize rows together instead
+	// of this crazy Dynamic hack.
+	for _, row := range s.table {
+		tags = append(tags, uint32(len(tags)))
+		vecs = append(vecs, s.materializeRow(row))
+	}
+	return vector.NewDynamic(tags, vecs)
+}
+
+func (s *superTable) materializeRow(row aggRow) vector.Any {
+	var vecs []vector.Any
+	for _, key := range row.keys {
+		vecs = append(vecs, vector.NewConst(key, 1, nil))
+	}
+	for _, fn := range row.funcs {
+		val := fn.Result()
+		vecs = append(vecs, vector.NewConst(val, 1, nil))
+	}
+	return s.builder.New(vecs)
+}
 
 //XXX need to make sure vam operator objects are returned to GC as they are finished
 
@@ -99,9 +192,9 @@ func (c *countByString) countFixed(vec *vector.Const) {
 	val := vec.Value()
 	switch val.Type().ID() {
 	case zed.IDString:
-		c.table[zed.DecodeString(val.Bytes())] += uint64(vec.Length())
+		c.table[zed.DecodeString(val.Bytes())] += uint64(vec.Len())
 	case zed.IDNull:
-		c.nulls += uint64(vec.Length())
+		c.nulls += uint64(vec.Len())
 	}
 }
 

runtime/vam/op/summarize/summarize.go
@@ -1,0 +1,109 @@
+package summarize
+
+import (
+	zed "github.com/brimdata/super"
+	"github.com/brimdata/super/pkg/field"
+	"github.com/brimdata/super/runtime/vam/expr"
+	"github.com/brimdata/super/vector"
+)
+
+type Summarize struct {
+	parent vector.Puller
+	zctx   *zed.Context
+	// XX Abstract this runtime into a generic table computation.
+	// Then the generic interface can execute fast paths for simple scenarios.
+	aggs      []*expr.Aggregator
+	aggNames  field.List
+	keyExprs  []expr.Evaluator
+	keyNames  field.List
+	typeTable *zed.TypeVectorTable
+	builder   *vector.RecordBuilder
+
+	types   []zed.Type
+	tables  map[int]aggTable
+	results []aggTable
+}
+
+func New(parent vector.Puller, zctx *zed.Context, aggNames field.List, aggs []*expr.Aggregator, keyNames []field.Path, keyExprs []expr.Evaluator) (*Summarize, error) {
+	builder, err := vector.NewRecordBuilder(zctx, append(keyNames, aggNames...))
+	if err != nil {
+		return nil, err
+	}
+	return &Summarize{
+		parent:    parent,
+		aggs:      aggs,
+		keyExprs:  keyExprs,
+		tables:    make(map[int]aggTable),
+		typeTable: zed.NewTypeVectorTable(),
+		types:     make([]zed.Type, len(keyExprs)),
+		builder:   builder,
+	}, nil
+}
+
+func (s *Summarize) Pull(done bool) (vector.Any, error) {
+	if done {
+		_, err := s.parent.Pull(done)
+		return nil, err
+	}
+	if s.results != nil {
+		return s.next(), nil
+	}
+	for {
+		//XXX check context Done
+		vec, err := s.parent.Pull(false)
+		if err != nil {
+			return nil, err
+		}
+		if vec == nil {
+			for _, t := range s.tables {
+				s.results = append(s.results, t)
+			}
+			s.tables = nil
+			return s.next(), nil
+		}
+		var keys, vals []vector.Any
+		for _, e := range s.keyExprs {
+			keys = append(keys, e.Eval(vec))
+		}
+		for _, e := range s.aggs {
+			vals = append(vals, e.Eval(vec))
+		}
+		vector.Apply(false, func(args ...vector.Any) vector.Any {
+			s.consume(args[:len(keys)], args[len(keys):])
+			// XXX Perhaps there should be a "consume" version of Apply where
+			// no return value is expected.
+			return vector.NewConst(zed.Null, args[0].Len(), nil)
+		}, append(keys, vals...)...)
+	}
+}
+
+func (s *Summarize) consume(keys []vector.Any, vals []vector.Any) {
+	var keyTypes []zed.Type
+	for _, k := range keys {
+		keyTypes = append(keyTypes, k.Type())
+	}
+	tableID := s.typeTable.Lookup(keyTypes)
+	table, ok := s.tables[tableID]
+	if !ok {
+		table = s.newAggTable()
+		s.tables[tableID] = table
+	}
+	table.update(keys, vals)
+}
+
+func (s *Summarize) newAggTable() aggTable {
+	return &superTable{
+		table:   make(map[string]aggRow),
+		aggs:    s.aggs,
+		builder: s.builder,
+	}
+}
+
+func (s *Summarize) next() vector.Any {
+	if len(s.results) == 0 {
+		return nil
+	}
+	t := s.results[0]
+	s.results = s.results[1:]
+	return t.materialize()
+}

vector/recordbuilder.go
@@ -1,0 +1,75 @@
+package vector
+
+import (
+	"slices"
+
+	zed "github.com/brimdata/super"
+	"github.com/brimdata/super/pkg/field"
+)
+
+type RecordBuilder struct {
+	zctx   *zed.Context
+	fields field.List
+	base   *rec
+}
+
+func NewRecordBuilder(zctx *zed.Context, fields field.List) (*RecordBuilder, error) {
+	base := &rec{}
+	for _, path := range fields {
+		if err := addPath(base, path); err != nil {
+			return nil, err
+		}
+	}
+	return &RecordBuilder{zctx: zctx, base: base}, nil
+}
+
+func (r *RecordBuilder) New(vecs []Any) *Record {
+	rec, _ := r.base.build(r.zctx, vecs)
+	return rec
+}
+
+type rec struct {
+	paths []string
+	recs  []*rec
+}
+
+func addPath(r *rec, path field.Path) error {
+	for k, name := range path {
+		idx := slices.Index(r.paths, name)
+		if k == len(path)-1 {
+			if idx > -1 {
+				return &zed.DuplicateFieldError{Name: path.String()}
+			}
+			r.paths = append(r.paths, path[k])
+			r.recs = append(r.recs, nil)
+			return nil
+		}
+		if idx == -1 {
+			idx = len(r.paths)
+			r.paths = append(r.paths, name)
+			r.recs = append(r.recs, &rec{})
+		}
+		if r.recs[idx] == nil {
+			return &zed.DuplicateFieldError{Name: path[:k+1].String()}
+		}
+		r = r.recs[idx]
+	}
+	return nil
+}
+
+func (r *rec) build(zctx *zed.Context, leafs []Any) (*Record, []Any) {
+	var fields []zed.Field
+	var out []Any
+	for i, name := range r.paths {
+		var vec Any
+		if r.recs[i] != nil {
+			vec, leafs = r.recs[i].build(zctx, leafs)
+		} else {
+			vec, leafs = leafs[0], leafs[1:]
+		}
+		fields = append(fields, zed.NewField(name, vec.Type()))
+		out = append(out, vec)
+	}
+	typ := zctx.MustLookupTypeRecord(fields)
+	return NewRecord(typ, out, out[0].Len(), nil), leafs
+}