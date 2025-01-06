15a291b4fd90034a9f2133d656baddb81d646265
Matthew Nibecker <hello@mattnibecker.com>
2025-01-06 11:43:50 -0800

    Update vector/union.go
    Co-authored-by: Noah Treuhaft <noah.treuhaft@gmail.com>

--- vector/union.go
+++ vector/union.go
@@ -26,13 +26,13 @@ func (u *Union) Type() super.Type {
 func (u *Union) Serialize(b *zcode.Builder, slot uint32) {
 	if u.Nulls.Value(slot) {
 		b.Append(nil)
-	} else {
-		b.BeginContainer()
-		tag := u.Typ.TagOf(u.TypeOf(slot))
-		b.Append(super.EncodeInt(int64(tag)))
-		u.Dynamic.Serialize(b, slot)
-		b.EndContainer()
+		return
 	}
+	b.BeginContainer()
+	tag := u.Typ.TagOf(u.TypeOf(slot))
+	b.Append(super.EncodeInt(int64(tag)))
+	u.Dynamic.Serialize(b, slot)
+	b.EndContainer()
 }
 
 func (u *Union) AppendKey(b []byte, slot uint32) []byte {
